a859318e8e9b2d209472e3b270a4340f1051082c
StuffToysSupremacy <159388907+StuffToysSupremacy@users.noreply.github.com>
2026-02-05 09:28:37 +1300

    Add files via upload

---
+++ C_01_Temp_Gui_v1.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+class Converter():
+    """
+    Temperature conversion tool (°C to °F or °f to °C)
+    """
+
+
+    def __init__(self):
+        """
+        Temperature Converter GUI
+        """
+
+    self.temp_frame - Frame(padx=10, pady=10)
+    self.temp_frame.grid()
+
+
+    # main routine
+
+
+if __name__ == "__main__":
+    root - Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
